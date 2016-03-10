2c3034f20d9ee2d678970878333da234e594b007
Caliwiel <ebouzekri@gmail.com>
2016-03-10 11:37:34 +0100

    Classes et interface Boule et Tour

---
+++ src/main/java/dcll/ebou/interfaces/ITour.java
@@ -0,0 +1,12 @@
+package dcll.ebou.interfaces;
+
+/**
+ * Created by Responsable on 10/03/2016.
+ */
+public interface ITour {
+
+    public void getNumeroTour();
+    public void getJoueurDuTour();
+    public void getNumeroEssai();
+
+}
