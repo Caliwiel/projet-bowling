e156a1fd2aeac00ce0ba98e9532b3638bdb59c11
Caliwiel <ebouzekri@gmail.com>
2016-03-10 11:56:05 +0100

    #22

--- src/main/java/dcll/ebou/impl/Boule.java
+++ src/main/java/dcll/ebou/impl/Boule.java
@@ -5,6 +5,7 @@ import dcll.ebou.interfaces.IBoule;
 /**
  * Created by Responsable on 10/03/2016.
  */
+  /*exigence 22*/
 public class Boule implements IBoule{
 
     public void lancer() {
--- src/main/java/dcll/ebou/interfaces/IBoule.java
+++ src/main/java/dcll/ebou/interfaces/IBoule.java
@@ -3,6 +3,7 @@ package dcll.ebou.interfaces;
 /**
  * Created by Responsable on 10/03/2016.
  */
+/* exigence 22 */
 public interface IBoule {
 
     public void lancer();
