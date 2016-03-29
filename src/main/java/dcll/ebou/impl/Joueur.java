0ea0998ded3e2795ab79467c3319f64556750129
Milila <cannac.anais@hotmail.fr>
2016-03-29 15:38:47 +0200

    #5 Partie sans tour, début de jeu lancer

--- src/main/java/dcll/ebou/impl/Joueur.java
+++ src/main/java/dcll/ebou/impl/Joueur.java
@@ -7,22 +7,56 @@ import dcll.ebou.interfaces.IJoueur;
  */
 public class Joueur implements IJoueur {
 
+    /**
+     * Nom du joueur
+     */
     private String name;
+
+    /**
+     * Numéro du joueur
+     */
     private int number;
+
+
+    //Je sais pas trop si ça va être utile
+    /**
+     * Score du joueur
+     */
     private int score;
 
+
+    /**
+     * on 29/03/2016
+     * Constructeur
+     * @param nom
+     * @param num
+     */
     public Joueur (String nom, int num) {
         this.name = nom;
         this.number = num;
         this.score = 0;
     }
 
+    /**
+     * Retourne le nom du joueur
+     * @return
+     */
     public String getName (){
         return name;
     }
 
+    /**
+     * Retourne le numéro du joueur
+     * @return
+     */
     public int getNumber (){
         return number;
     }
+
+    /**
+     * Retourne le score du joueur
+     * @return
+     */
+    public int getScore ()  { return score; }
 
 }
